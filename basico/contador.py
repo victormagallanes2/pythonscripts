723fe12fb6fefa2db5ec4be3dabbd4788edd37b2
victormagallanes2 <victormagallanes2@gmail.com>
2020-03-08 13:35:02 -0400

    s

---
+++ basico/contador.py
@@ -0,0 +1,6 @@
+contador = 0
+while contador <= 8:
+    print("vuelta", str(contador))
+    contador += 1
+    if contador == 8:
+        contador = 0
---
+++ lib_standart/lib_asyncio.py
@@ -0,0 +1,25 @@
+import asyncio
+
+# definimos la función
+async def show_seconds():
+    "Segundos"
+    while True:
+        for i in range(60):
+            print(i, 's')
+            await asyncio.sleep(1)
+
+
+async def show_minute():
+    for i in range(1, 10):
+        await asyncio.sleep(60)
+        print(i, 'minuto')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(
+    # Con gather, podemos ejecutar las dos rutinas 
+    # simultaneamente
+    asyncio.gather(show_seconds(),
+                   show_minute())
+)
+loop.close()
